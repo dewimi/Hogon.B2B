d02c91d5189147f0ffdbad223a8e9e2b3f06f4d2
DewiMi <582206262@qq.com>
2017-08-18 14:54:08 +0800

    增加下拉表格

--- Code/Hogon.Store.Models/Hogon.Store.Models.FluentAPI/Migrations/201708160538493_menuupdate.cs
+++ Code/Hogon.Store.Models/Hogon.Store.Models.FluentAPI/Migrations/201708160538493_menuupdate.cs
@@ -9,7 +9,6 @@ namespace Hogon.Store.Models.FluentAPI.Migrations
         {
             AddColumn("dbo.Menus", "Sort", c => c.Int(nullable: false));
             AlterColumn("dbo.Menus", "Code", c => c.String(nullable: false, maxLength: 50));
-            CreateIndex("dbo.Menus", "Code", unique: true, name: "MenuCodeIndexer");
         }
         
         public override void Down()
